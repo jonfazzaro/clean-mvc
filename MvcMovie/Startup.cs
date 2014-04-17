61aed174b8818f5ea561d27268fc90f8a0dd3b2a
jonfazzaro <jon@fazzaro.com>
2014-04-17 14:24:54 -0400

    Before.

---
+++ MvcMovie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMovie.Web.Startup))]
+namespace MvcMovie.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
